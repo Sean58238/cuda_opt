1fc6f1db71cfc76237372aaf0c7d6176194f05a8
Sean58238 <xiang.lin@intel.com>
2026-06-23 00:33:47 +0000

    add makefile

--- basic/hello_gpu.cu
+++ basic/hello_gpu.cu
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//core funcation, return type void
 __global__ void hello_from_gpu()
 {
     printf("Hello world, from GPU!\n");
@@ -7,12 +8,13 @@ __global__ void hello_from_gpu()
 
 int main(void)
 {
+    //call from CPU
     printf("Hello world, from CPU!\n");
 
     //call core funcation, allocat 1 block, 10 threads
     hello_from_gpu<<<1, 10>>>();
      
-    //syncchronize fo host and device
+    //syncchronize fo host and device, refresh out buffer
     cudaDeviceSynchronize();
 
     return 0;
